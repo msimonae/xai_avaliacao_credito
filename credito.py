7b35f15f3bceccf6dd5b6073e3d9d6927ccdfb02
Marcelo Massashi Simonae <msimonae@gmail.com>
2025-10-06 10:57:34 -0300

    Update credito.py
    V1.9

--- credito.py
+++ credito.py
@@ -475,7 +475,10 @@ if st.button("Verificar Crédito"):
 Você é um Cientista de Dados Sênior, especialista em explicar os resultados de modelos de Machine Learning para clientes de forma clara, objetiva e humana.
 O modelo de análise de crédito previu o resultado '{resultado_texto}' para um cliente.
 
-Aqui estão as explicações técnicas sobre os fatores que mais influenciaram essa decisão e se for recusado explique os motivos da recuso, mas se for aprovado parabenize o cliente :
+Aqui estão as explicações técnicas sobre os fatores que mais influenciaram essa decisão:
+
+Se o crédito foi recusado, forneça de 3 até 5 dicas práticas sobre como o cliente pode melhorar seu perfil.
+
 - **SHAP (Contribuição dos atributos):**
 {exp_rec_shap}
 - **LIME (Regras de decisão):**
@@ -483,7 +486,9 @@ Aqui estão as explicações técnicas sobre os fatores que mais influenciaram e
 
 Com base nas informações do **SHAP** e **LIME**, crie um feedback amigável para o cliente, seguindo as instruções abaixo:
 
-1.  **Análise do Resultado:** De forma amigável e empática, explique todas as principais motivos que levaram à decisão. Mencione todos os fatores do SHAP e todas as regras do LIME e **liste em bullet points**. Para todos os resultado do LIME, explique em linguagem natural como a condição do fator influenciaram o resultado '{resultado_texto}' e compare com o seu limite da regra do LIME {exp_rec_lime}. Formate valores monetários com R$ e use vírgulas e pontos decimais de forma correta (Exemplo: R$ 50.000,00).
+1.  **Análise do Resultado:** De forma amigável e empática, explique todas as principais motivos que levaram à decisão. 
+Se o crédito foi recusado, Diga que seu crédito foi recusado e se foi aprovado, parabenize o cliente.
+Mencione todos os fatores do SHAP e todas as regras do LIME e **liste em bullet points**. Para os resultados SHAP o cuidado para não formatar as pontuações das variáveis importantes.Para todos os resultado do LIME, explique em linguagem natural como a condição do fator influenciaram o resultado '{resultado_texto}' e compare com o seu limite da regra do LIME {exp_rec_lime}. Formate valores monetários com R$ e use vírgulas e pontos decimais de forma correta (Exemplo: R$ 50.000,00).
 
 2.  **Pontos a Melhorar (se o resultado for 'Recusado')**: Se o crédito foi recusado, Diga que seu crédito foi recusado e forneça 2 ou 3 dicas práticas e acionáveis sobre como o cliente pode melhorar seu perfil para aumentar as chances de aprovação no futuro. Se foi aprovado, parabenize o cliente e apenas reforce os pontos positivos.
 
